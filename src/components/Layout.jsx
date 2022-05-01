3e0a84457d821b67c2e2b00131e1db99dae9f716
angel <angelriera1796@gmail.com>
2022-05-01 15:59:46 -0400

    call api funtion

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,7 +8,7 @@ import { useEffect } from 'react'
 export default function Layout({ title, description, children }) {
     const userStatus = useSelector(e => e.auth)
     const dispatch = useDispatch()
-    
+
     const check_user = () => {
         if (dispatch && dispatch !== undefined && dispatch !== null) {
             dispatch(auth_action())
@@ -24,7 +24,7 @@ export default function Layout({ title, description, children }) {
         <>
             <Head>
                 <meta charset='UTF-8' />
-                <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+                <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
                 <meta name='viewport' content='width=device-width, initial-scale=1.0' />
                 <meta name='description' content={description} />
                 <title>{title}</title>
